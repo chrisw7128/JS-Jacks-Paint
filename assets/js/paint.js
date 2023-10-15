57b072c279e561f4d0a4303b139e4f8a7f79cb9d
Chris Wehner <chrisw7128@gmail.com>
2023-10-15 10:40:57 -0700

    fixed javascript

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,27 +1,28 @@
 function configureListeners() {
-  let images = document.querySelectorAll("img");
+  // select img elements for use
+  let images = document.getElementsByTagName("img");
 
+  // iterate over images and add mouseover event listeners
   for (var i = 0; i < images.length; i++) {
-    document
-      .getElementById(images[i].id)
-      .addEventListener("mouseover", addOpacity, false);
-    document
-      .getElementById(images[i].id)
-      .addEventListener("mouseout", removeOpacity, false);
+    images[i].addEventListener("mouseover", removeOpacity);
+  }
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener("mouseout", addOpacity);
   }
 }
 
 function addOpacity(event) {
-  if (!this.classList.contains("dim")) {
-    this.classList.add("dim");
-  }
-  getProductInfo(event.target.id);
+  // add appropriate CSS classes
+  this.style.opacity = 1;
+  this.style.transform = "none";
 }
 
 function removeOpacity(event) {
-  if (this.classList.contains("dim")) {
-    this.classList.remove("dim");
-  }
+  //remove appropriate CSS classes
+  this.style.opacity = 0.5;
+  this.style.transform = "scale(1.1)";
+  this.style.transform = "translateZ(9999)";
+  this.style.transition = "all 0.3s";
 
   let element = document.getElementById("color-price");
   element.textContent = "";
@@ -30,6 +31,7 @@ function removeOpacity(event) {
   color.textContent = "";
 
   event.preventDefault();
+  getProductInfo(event.target.id);
 }
 
 function getProductInfo(paintColor) {
@@ -38,55 +40,46 @@ function getProductInfo(paintColor) {
 
   switch (paintColor) {
     case "pn1":
-      // set variables for price and color name and invoke a function to update the price
       price = "$14.99";
       colorName = "Lime Green";
       updatePrice(colorName, price);
       break;
     case "pn2":
-      // set variables for price and color name and invoke a function to update the price
       price = "$11.14";
       colorName = "Medium Brown";
       updatePrice(colorName, price);
       break;
     case "pn3":
-      // set variables for price and color name and invoke a function to update the price
       price = "$22.99";
       colorName = "Royal Blue";
       updatePrice(colorName, price);
       break;
     case "pn4":
-      // set variables for price and color name and invoke a function to update the price
-      price = "$4.99";
-      colorName = "Solid Black";
-      updatePrice(colorName, price);
-      break;
-    case "pn5":
-      // set variables for price and color name and invoke a function to update the price
-      price = "$8.22";
-      colorName = "Solid Cyan";
-      updatePrice(colorName, price);
-      break;
-    case "pn6":
-      // set variables for price and color name and invoke a function to update the price
-      price = "$11.99";
-      colorName = "Solid Purple";
-      updatePrice(colorName, price);
-      break;
-    case "pn7":
-      // set variables for price and color name and invoke a function to update the price
-      price = "$13.42";
+      price = "13.42";
       colorName = "Solid Red";
       updatePrice(colorName, price);
       break;
-    case "pn8":
-      // set variables for price and color name and invoke a function to update the price
+    case "pn5":
       price = "$21.98";
       colorName = "Solid White";
       updatePrice(colorName, price);
       break;
+    case "pn6":
+      price = "$4.99";
+      colorName = "Solid Black";
+      updatePrice(colorName, price);
+      break;
+    case "pn7":
+      price = "$8.22";
+      colorName = "Solid Cyan";
+      updatePrice(colorName, price);
+      break;
+    case "pn8":
+      price = "$11.99";
+      colorName = "Solid Purple";
+      updatePrice(colorName, price);
+      break;
     case "pn9":
-      // set variables for price and color name and invoke a function to update the price
       price = "$14.99";
       colorName = "Solid Yellow";
       updatePrice(colorName, price);
@@ -95,10 +88,14 @@ function getProductInfo(paintColor) {
   }
 
   function updatePrice(colorName, price) {
+    // select element with corresponding id
     let colorPrice = document.getElementById("color-price");
-    console.log(colorPrice);
+    // display price
+    colorPrice.textContent = price;
 
+    // select element with corresponding id
     let color = document.getElementById("color-name");
-    console.log(color);
+    //display color name
+    color.textContent = colorName;
   }
 }
